05e7a2ce2c9569366eb8421992067537f6928fd9
Gary <gary.github@gmail.com>
2021-11-25 20:35:44 +0800

    Update color.py

--- color.py
+++ color.py
@@ -1,19 +1,13 @@
 import zlib
 
+b4 = lambda i : i.to_bytes(4, 'big')
 
-# common functions
-def b4(i: int): return i.to_bytes(4, 'big')
-def chunk(b: bytes): return b4(len(b) - 4) + b + b4(zlib.crc32(b))
-
-
-# constants
 SIG = b'\x89PNG\r\n\x1a\n'
-IHDR = b'IHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x02\x00\x00\x00'
+IHDR = b'\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x02\x00\x00\x00\x90wS\xde'
 IEND = b'\x00\x00\x00\x00IEND'
 
-
 def png(h: int) -> None:
-    out = f'{hex(h)[2:]}.png'
-    IDAT = b'IDAT' + zlib.compress(b4(h))
-    data = SIG + chunk(IHDR) + chunk(IDAT) + IEND
-    open(out, 'wb').write(data)
+    out = hex(h)[2:] + '.png'
+    s = zlib.compress(b4(h))
+    IDAT = b4(len(s)) + b'IDAT' + s + b4(zlib.crc32(s))
+    open(out, 'wb').write(SIG + IHDR + IDAT + IEND)
