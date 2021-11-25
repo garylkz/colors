d5fb4a721f4aac7d74239b961685edb48ab8171c
Gary <gary.github@gmail.com>
2021-11-26 02:01:00 +0800

    Simplicity is the ultimate form of sophistication

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-const hex = document.getElementById('hex');
-const _ = document.getElementById('download');
+const c = document.getElementById('c');
+const h = document.getElementById('h');
+const d = document.getElementById('d');
 
-const SIG = [137, 80, 78, 71, 13, 10, 26, 10];
-const IHDR = [00, 00, 00, 13, 73, 72, 68, 82, 
-              00, 00, 00, 01, 00, 00, 00, 01, 
-              08, 02, 00, 00, 00];
-const IEND = [00, 00, 00, 00, 73, 69, 78, 68];
-var idat = [73, 68, 65, 84];
-
-function test() {
-    const h = hex.value;
-    var blob = process(h);
-    download(blob, h);
+function fill() {
+    var ctx = c.getContext('2d');
+    ctx.fillStyle = '#' + h.value;
+    ctx.fillRect(0, 0, 1, 1);
 }
 
-function process(chars) {
-    var uint8 = new Uint8Array(4);
-    for (let i = 0; i < 3; i++) {
-        uint8[i + 1] = parseInt(chars.substring(i * 2, (i + 1) * 2), 16);
-    }
-    // TODO : zlib, crc32
-    var IDAT = [];
-    var byteArray = SIG.concat(IHDR, IDAT, IEND);
-    return new Blob(byteArray, {type: "octet/stream"});
-}
-
-function download(obj, name) {
-    _.href = window.URL.createObjectURL(obj);
-    _.download = name + '.txt';
-    _.click();
-    window.URL.revokeObjectURL(_.href);
+function download() {
+    d.href = c.toDataURL();
+    d.download = h.value + '.png';
+    d.click();
 }
